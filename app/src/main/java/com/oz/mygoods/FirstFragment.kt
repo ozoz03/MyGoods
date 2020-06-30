6e70d4a2d4d2439ff3e004a54d5bd8124e4d3f19
ozasymenko <oleg.zasymenko@vgs.io>
2020-06-30 19:53:35 +0300

    checked listview

--- app/src/main/java/com/oz/mygoods/FirstFragment.kt
+++ app/src/main/java/com/oz/mygoods/FirstFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.BaseAdapter
 import android.widget.Button
+import android.widget.CheckedTextView
 import android.widget.ListView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -47,20 +49,31 @@ class FirstFragment : Fragment() {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
 
-        val listItems: List<String> = mutableListOf()
+        val goods: List<Good> = mutableListOf()
         val goodAdapter =
-            ArrayAdapter(this.requireContext(), android.R.layout.simple_list_item_1, listItems)
+            ArrayAdapter(this.requireContext(), android.R.layout.simple_list_item_checked, goods)
         val listView = view.findViewById<ListView>(R.id.order_list_view)
         listView.adapter = goodAdapter
+        listView.choiceMode = ListView.CHOICE_MODE_MULTIPLE
+
+        listView.onItemClickListener =
+            AdapterView.OnItemClickListener { parent, view, position, id ->
+//                Log.i(TAG, "onItemClick: $position")
+                val v = view as CheckedTextView
+                val currentCheck = v.isChecked
+                val user: Good = listView.getItemAtPosition(position) as Good
+                user.needed = (!currentCheck)
+            }
         view.findViewById<Button>(R.id.button_load).setOnClickListener {
             val createCall = service.needed()
             createCall.enqueue(object : Callback<List<Good>> {
                 override fun onResponse(
                     call: Call<List<Good>>, response: Response<List<Good>>
                 ) {
-                    (listItems as MutableList).clear()
-                    for (item in response.body()) {
-                        listItems.add(item.toString())
+                    (goods as MutableList).clear()
+                    for ((i, item) in response.body().withIndex()) {
+                        goods.add(item)
+                        listView.setItemChecked(i, item.needed?: false)
                     }
                     (listView.adapter as BaseAdapter).notifyDataSetChanged()
                 }
